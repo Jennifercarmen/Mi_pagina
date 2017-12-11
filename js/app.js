c78607c68890768e6dcba804508d7cce757e0a1c
Jennifercarmen <jennifercarmens@gmail.com>
2017-12-11 09:22:55 -0500

    image responsive

---
+++ js/app.js
@@ -0,0 +1,19 @@
+
+window.addEventListener("load", function () {
+    var icon = document.getElementById("navigation");
+    var contentmenu = document.getElementById("content-menu");
+    
+	icon.addEventListener("click",function(event){
+		var tarjeta = document.createElement("input");
+		var div_content = document.createElement("div");		
+		var btn_guardar = document.createElement("button");
+		var btn_closet=document.createElement("span");
+		
+        alert("Menu hamburguesa")
+
+ 
+})
+
+
+
+});
